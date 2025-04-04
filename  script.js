0fd0ab5144eccf2d69348e07aff582ca459ed59b
jeniffer2303 <jeniffer.santos.moraes@escola.pr.gov.br>
2025-04-03 21:02:14 -0300

    Add files via upload

---  script.js
+++  script.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
-    const aumentaFonteBotao "document.get"
-    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        DocumentTimeline.body.style.fontsize = '${tamanhoAtualFonte}rem'
+
+    })
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        DocumentTimeline.body.style.fontsize = '${tamanhoAtualFonte}rem'
+
+})
+
 
 })
